5341b677288a4e15bd3cab3a23b801dd57208a58
Qingzhi Luo <42762197+tomrony@users.noreply.github.com>
2018-09-21 23:55:53 -0700

    Question1

---
+++ Telephone.java
@@ -0,0 +1,61 @@
+package hw2;
+
+import java.util.Scanner;
+
+public class Telephone {
+	/**
+	 * Construct phone object.
+	 * 
+	 * @param aScanner
+	 *            that reads text from a character-input stream
+	 */
+	public Telephone(Scanner aScanner) {
+		scanner = aScanner;
+	}
+
+	/**
+	 * Speak a message to System.out.
+	 * 
+	 * @param output
+	 *            the text that will be "spoken"
+	 */
+	public void speak(String output) {
+		System.out.println(output);
+	}
+
+	/**
+      Loops reading user input and passes the input to the
+      Connection object's methods dial, record or hangup.
+      @param c the connection that connects this phone to the
+      voice mail system
+   */
+   public void run(Connection c)
+   {
+      boolean more = true;
+      while (more)
+      {
+         String input = scanner.nextLine();
+//         if("1234567890#HQ".contains(input))
+//         {
+         if (input == null) return;
+         if (input.equalsIgnoreCase("H"))
+            c.hangup();
+         else if (input.equalsIgnoreCase("Q"))
+            more = false;
+         else if (input.length() == 1
+            && "1234567890#".contains(input))
+            c.dial(input);
+         else
+            c.record(input);
+      
+//      }
+//      else
+//      {
+//    	  System.out.println("invalid entry");
+//    	  System.out.println("Enter mailbox number followed by #");
+//      }
+      }
+   }
+
+	private Scanner scanner;
+}
